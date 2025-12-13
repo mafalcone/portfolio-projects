4795acf540b94de16902bbf8f96b48953cb51808
Manu Falcone <manu.axl@hotmail.com>
2025-12-13 16:30:54 -0300

    Add debug boundary to reveal runtime error

--- taskpulse_frontend/src/main.jsx
+++ taskpulse_frontend/src/main.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AuthProvider } from "./contexts/AuthContext";
+import DebugBoundary from "./DebugBoundary";
 import "./styles/globals.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <DebugBoundary>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </DebugBoundary>
   </React.StrictMode>
 );
