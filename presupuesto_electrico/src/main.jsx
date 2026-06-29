42306639ebf51fb1dbc409b8f82f98bfba56cf04
mafalcone <32884392+mafalcone@users.noreply.github.com>
2026-06-29 12:39:37 -0300

    Use portfolio demos as public app

--- presupuesto_electrico/src/main.jsx
+++ presupuesto_electrico/src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import Portfolio from './Portfolio'
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Portfolio />
   </React.StrictMode>
 )
