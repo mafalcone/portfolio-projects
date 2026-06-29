59b2ffaf523e55a2ba494ac692b1c260c32c0bd1
mafalcone <32884392+mafalcone@users.noreply.github.com>
2026-06-29 16:21:56 -0300

    Use clean public portfolio

--- presupuesto_electrico/src/main.jsx
+++ presupuesto_electrico/src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Portfolio from './Portfolio'
+import PublicPortfolio from './PublicPortfolio'
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Portfolio />
+    <PublicPortfolio />
   </React.StrictMode>
 )
